6bd0f84eaac98242f565e0489ea80f3b9c9052ba
mtang24 <michaeljtang18@gmail.com>
2025-04-04 19:27:28 -0700

    Add snap tutorial

--- global.js
+++ global.js
@@ -1,3 +1,41 @@
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'snap_tutorial/', title: 'Snap Tutorial' },
+  // add the rest of your pages here
+
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  const ARE_WE_HOME = document.documentElement.classList.contains('home');
+  if (!ARE_WE_HOME && !url.startsWith('http')) {
+    url = '../' + url;
+  }
+
+  // Create link and add it to nav
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+
+  if (a.host != location.host) {
+    a.target = "_blank";
+  }
+}
+
 document.body.insertAdjacentHTML(
     'afterbegin',
     `
